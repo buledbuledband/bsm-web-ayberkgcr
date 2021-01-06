1126ad4fc82f824cbe7bf50c70f974a053dd45f3
davudgunay <59139301+davudgunay@users.noreply.github.com>
2021-01-07 01:08:53 +0300

    webprog
    davudgunay

--- WebApp/Models/hakkimiza.cs
+++ WebApp/Models/hakkimiza.cs
@@ -16,7 +16,7 @@ namespace WebApp.Models
     {
         public int id { get; set; }
         public string foto { get; set; }
-        public string baslik { get; set; }
+        public string baslig { get; set; }
         public string usttbaslik { get; set; }
         public string icerik { get; set; }
     }
--- WebApp/Models/kahve2020.Designer.cs
+++ WebApp/Models/kahve2020.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\FO\source\repos\WebApp\WebApp\Models\kahve2020.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\user\Documents\GitHub\bsm-web-ayberkgcr\WebApp\Models\kahve2020.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
