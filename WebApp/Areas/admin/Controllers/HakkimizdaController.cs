7c1458572ca51cc8ba21a2b39586e14c57df2ee0
davudgunay <59139301+davudgunay@users.noreply.github.com>
2021-01-07 21:27:35 +0300

    webprog
    davudgunay

---
+++ WebApp/Areas/admin/Controllers/HakkimizdaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Areas.admin.Controllers
+{
+    [Authorize]
+    public class HakkimizdaController : Controller
+    {
+        // GET: admin/Hakkimizda
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
